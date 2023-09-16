cbe125d54ed8aae4d7452a503c63a51e7c518f43
sween <khansween@gmail.com>
2023-09-17 04:43:44 +0530

    added category route

---
+++ models/reciepeImageSchema.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const imageSchema = new mongoose.Schema({
+    categoryName :{
+        type: String,
+        required: true
+    }, categoryImg :{
+        type: String,
+        required: true
+    }
+})
+
+const categoryImage = mongoose.model("categoryImage", imageSchema)
+
+export default categoryImage
