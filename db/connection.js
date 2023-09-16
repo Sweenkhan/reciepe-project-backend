7375d21e18ffe988fe8bf990b05da7f12f7227d4
sween <khansween@gmail.com>
2023-09-16 23:39:34 +0530

    added connection with mongodb

---
+++ db/connection.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+const connection = mongoose.connect("mongodb+srv://sween:sween_1997@cluster1.lw5cggq.mongodb.net/?retryWrites=true&w=majority")
+
+
+export default connection
