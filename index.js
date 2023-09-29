cb38b44030120075369887377af85a3d7f2d0001
sween <khansween@gmail.com>
2023-09-30 01:52:32 +0530

    added

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from "express"
 import connection from "./db/connection.js";
-import cors from "cors";  
-import categoryImage from "./models/reciepeImageSchema.js";
-import reciepe
+import cors from "cors";   
+import reciepeRoute from "./routes/reciepeRoute.js";
 
 const app = express()
 
@@ -11,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({ origin: "http://localhost:5173" }))
 
-// app.use(reciepeRoute) 
+app.use(reciepeRoute) 
 
 
 
@@ -20,17 +19,7 @@ app.get("/", async(req, res) => {
   });
 
   
-
-app.get("/getAllCategory", async(req, res)=> {
- 
-    let allCategory = await categoryImage.find({})
-    console.log("heeeeeelo")
-    console.log(allCategory)
-    res.send({status: 200, message: "getting all category."})
-
-})  
-
-
+  
 connection
     .then(() => {
         app.listen(8000, () => {
--- routes/reciepeRoute.js
+++ routes/reciepeRoute.js
@@ -1,8 +1,30 @@
  import express from "express";
+ import categoryImage from "../models/reciepeImageSchema.js";
+ import reciepe from "../models/reciepeSchema.js";
 
  const router = express.Router()
 
 
- router.get("/getAllReciepe", async(req, res)=> {
+ router.get("/getAllCategory", async(req, res)=> {
+ 
+    let allCategory = await categoryImage.find({})
+    console.log("heeeeeelo")
+    // console.log(allCategory)
+    res.send({status: 200, message: "getting all category.", allCategory: allCategory})
 
- })
+})  
+
+
+router.get("/getSelectedCategory/:name", async(req, res) => {
+
+    console.log(req.params.name)
+    let category = req.params.name
+
+   let selectedCatagoryData =  await reciepe.find({category})
+
+   console.log(selectedCatagoryData)
+    res.send({status: 200, message: "getting selected category data.", selectedCatagoryData})
+
+})
+
+export default router
