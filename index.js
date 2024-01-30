3cdba629a308db9e48dd2b8d4dbb004fbfe43227
sween <khansween@gmail.com>
2024-01-31 00:00:06 +0530

    a

--- index.js
+++ index.js
@@ -1,41 +1,85 @@
-import express from "express"
+import express from "express";
 import connection from "./db/connection.js";
-import cors from "cors";   
+import cors from "cors";
 import reciepeRoute from "./routes/reciepeRoute.js";
-// import multer from "multer";
-
-const app = express()
+import nodemailer from "nodemailer"; 
 
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors({ origin: "http://localhost:5173" }))
 
-app.use(reciepeRoute) 
+const app = express();
 
-app.get("/", async(req, res) => {
-    // res.status(200).sendFile(__dirname + "main.js")
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "http://localhost:5173" }));
+
+app.use(reciepeRoute);
+
+app.get("/", async (req, res) => {
+  // res.status(200).sendFile(__dirname + "main.js")
+});
+
+ 
+
+app.post("/contact", async(req, res) => {
+    const formData = req.body;
+
+  // // Create a Nodemailer transporter using SMTP details
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+        auth: {
+      user: "khansween@gmail.com",
+      pass: "ykfzkxevhwhwmmql",
+    },
   });
 
+  const mailOptions = {
+      from: 'khansween@gmail.com',
+    to: "kbhai1585@gmail.com",
+    subject: "New Contact Form Submission",
+    html: `
+      <p><strong>Name:</strong> ${formData.firstName} ${formData.lastName}</p>
+      <p><strong>Email:</strong> ${formData.email}</p>
+      <p><strong>Phone:</strong> ${formData.phone}</p>
+      <p><strong>Message:</strong> ${formData.message}</p>
+    `,
+  };
 
 
-// const storage = multer.diskStorage({
-//     destination: function(req, file, cb){
-//           return cb(null, "./images/photos")
-//     },filename: function(req, file, cb){
-//         return cb(null, `${Date.now()}_${file.originalname}`)
-//     }
-// })  
-// const upload = multer({storage})
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: ", info.response);
 
-app.post("/contact", (req, res) => { 
-    console.log(req.body)
-    res.send({status: 200, message: "everything is done!"})
-})  
+    if(info){
+        const mailOptions2 = {
+            from: 'khansween@gmail.com',
+            to: formData.email,
+            subject: "Thank You for Contacting Reciepe-Maker", 
+               html: `
+            <p>Dear <strong>${formData.firstName} ${formData.lastName}</strong>,</p>
+            <p>Thank you for reaching out to us! We appreciate the time you took to fill out the contact form on our website. Your inquiry is important to us, and we are committed to addressing it as quickly as possible.</p>
 
-connection
-    .then(() => {
-        app.listen(8000, () => {
-            console.log("server is running on port 8000")
-        })
-    })
+           <p> Our team is currently reviewing your message, and we will get back to you shortly. In the meantime, if your matter is urgent, please feel free to contact us directly at <strong>khansween@gmail.com</strong>.
+            
+            We strive to provide excellent service and look forward to assisting you with [briefly mention the nature of their inquiry].</p>
+           `
+        }
+
+        const info2 = await transporter.sendMail(mailOptions2);
+        console.log("Email sent: ", info2.response);
+        return true
+    }
+    return true;
+  } catch (error) {
+    console.error("Error sending email: ", error);
+    return false;
+  }
+  res.send({ status: 200, message: "everything is done!" });
+});
+
+
+
+connection.then(() => {
+  app.listen(8000, () => {
+    console.log("server is running on port 8000");
+  });
+});
--- nodemail/nodemailer.js
+++ nodemail/nodemailer.js
@@ -1,42 +1,61 @@
-// import nodemailer from "nodemailer"
+import nodemailer from "nodemailer"; 
 
-// // Create a Nodemailer transporter using SMTP details
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: 'your-email@gmail.com',
-//     pass: 'your-email-password',
-//   },
-// });
 
-// // Function to send an email
-// const sendEmail = async (formData) => {
-//   const mailOptions = {
-//     from: 'your-email@gmail.com',
-//     to: 'recipient-email@example.com',
-//     subject: 'New Contact Form Submission',
-//     html: `
-//       <p><strong>Name:</strong> ${formData.firstName} ${formData.lastName}</p>
-//       <p><strong>Email:</strong> ${formData.email}</p>
-//       <p><strong>Phone:</strong> ${formData.phone}</p>
-//       <p><strong>Message:</strong> ${formData.message}</p>
-//     `,
-//     attachments: [
-//       {
-//         filename: formData.file.originalname,
-//         content: formData.file.buffer,
-//       },
-//     ],
-//   };
+const creatNodeMail = async(req, res) => {
+    const formData = req.body;
 
-//   try {
-//     const info = await transporter.sendMail(mailOptions);
-//     console.log('Email sent: ', info.response);
-//     return true;
-//   } catch (error) {
-//     console.error('Error sending email: ', error);
-//     return false;
-//   }
-// };
+  // // Create a Nodemailer transporter using SMTP details
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+        auth: {
+      user: "khansween@gmail.com",
+      pass: "ykfzkxevhwhwmmql",
+    },
+  });
 
-//  export default sendEmail;
+  const mailOptions = {
+      from: 'khansween@gmail.com',
+    to: "kbhai1585@gmail.com",
+    subject: "New Contact Form Submission",
+    html: `
+      <p><strong>Name:</strong> ${formData.firstName} ${formData.lastName}</p>
+      <p><strong>Email:</strong> ${formData.email}</p>
+      <p><strong>Phone:</strong> ${formData.phone}</p>
+      <p><strong>Message:</strong> ${formData.message}</p>
+    `,
+  };
+
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: ", info.response);
+
+    if(info){
+        const mailOptions2 = {
+            from: 'khansween@gmail.com',
+            to: formData.email,
+            subject: "Thank You for Contacting Reciepe-Maker", 
+               html: `
+            <p>Dear <strong>${formData.firstName} ${formData.lastName}</strong>,</p>
+            <p>Thank you for reaching out to us! We appreciate the time you took to fill out the contact form on our website. Your inquiry is important to us, and we are committed to addressing it as quickly as possible.</p>
+
+           <p> Our team is currently reviewing your message, and we will get back to you shortly. In the meantime, if your matter is urgent, please feel free to contact us directly at <strong>khansween@gmail.com</strong>.
+            
+            We strive to provide excellent service and look forward to assisting you with [briefly mention the nature of their inquiry].</p>
+           `
+        }
+
+        const info2 = await transporter.sendMail(mailOptions2);
+        console.log("Email sent: ", info2.response);
+        return true
+    }
+    return true;
+  } catch (error) {
+    console.error("Error sending email: ", error);
+    return false;
+  }
+  res.send({ status: 200, message: "everything is done!" });
+};
+
+
+export default creatNodeMail;
