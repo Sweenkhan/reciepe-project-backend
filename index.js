73eaf3de50fd699838349b7b25fbe6fd6ebb1a3e
sween <khansween@gmail.com>
2024-03-22 18:17:36 +0530

    tryin still

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.use(cors({
   "optionsSuccessStatus": 204
  })); 
 
+ app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
