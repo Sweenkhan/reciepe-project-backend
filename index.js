aabd568bf50935d29a7d6043f7fcdbfeb95d5f97
sween <khansween@gmail.com>
2024-03-24 00:21:44 +0530

    trying again

--- index.js
+++ index.js
@@ -7,18 +7,20 @@ import creatNodeMail from "./nodemail/nodemailer.js";
 const app = express();
 
 app.use(cors({
-  "origin": "https://qreciepe.onrender.com",
+  "origin": "*",
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": false,
   "optionsSuccessStatus": 204
  })); 
 
- app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next()
-});
-
+ 
+//  app.all('/', function(req, res, next) {
+//    res.header("Access-Control-Allow-Origin", "*");
+//    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//    next()
+//   });
+  
+  // app.use(cors({origin: "http://localhost:5173"}))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
