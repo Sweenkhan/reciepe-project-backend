68831fafff4a4a43d77a4ddec1a2a70d6d0cb1ac
Sween Khan <136142278+Sweenkhan@users.noreply.github.com>
2025-03-29 00:37:20 +0530

    Update connection file

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { config } from "dotenv";
 config()
 
-const connection = mongoose.connect(process.env.MONGO_URL)
+const connection = mongoose.connect("mongodb+srv://sween:sween_1997@cluster1.lw5cggq.mongodb.net/?retryWrites=true&w=majority")
 
 export default connection;
 
